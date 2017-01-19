883ecafa001bc2ec32cc3c34fcc0d2349e76bff8
Cori Grohman <corigrohman@gmail.com>
2017-01-18 22:00:08 -0600

    feat: updated call to bets.exacta(), added the list DIFF to enable multiple runs testing different 'omission' multipliers. Added argparse to enable passing of specific track/date/race parameters

--- betTestingPony.py
+++ betTestingPony.py
@@ -8,30 +8,45 @@ import pdb
 import logging
 from logging.config import dictConfig
 import env_config
+import argparse
 
 dictConfig(env_config.logging_config)
 logger = logging.getLogger()
 
 def main():
-    DIFF=2.5
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-date', dest='date', default=r'[0-9]+')
+    parser.add_argument('-track', dest='track', default=r'^[a-z]+')
+    parser.add_argument('-race', dest='race', default=None)
+    args = parser.parse_args()
+    
+    DIFF=[1,1.25,1.5, 1.75,2,2.25,2.5]
     testing_list = []
     for f in glob('./data/*'):
         csv_name = f.split('/')[2]
-        date = re.search(r'[0-9]+', csv_name).group()
-        track = re.match(r'[a-z]+[0-9]',csv_name).group()[:-1]
+        if not re.search(args.date, csv_name) or not re.match(args.track,csv_name): continue
+        date = re.search(args.date, csv_name).group()
+        track = re.match(args.track,csv_name).group()
         testing_list.append((date,track.upper()))
+ 
 
-    statc = Stats(starting_bank=400)
-    statd = Stats(starting_bank=400)
-    for day in testing_list:
-        races = Race.findRaces(date=day[0],track=day[1])
-        if not races:
-            logger.error('Could not find races: {} {}'.format(day[0], day[1]))
-        bets.one_two_overall(races, statc, DIFF)
-        bets.two_three_overall(races, statd, DIFF)
+    stata_list = []
+    statb_list = []
+    for diff in DIFF:
+        stata = Stats(starting_bank=400)
+        statb = Stats(starting_bank=400)
+        for day in testing_list:
+            races = Race.findRaces(date=day[0],track=day[1], race_number=args.race)
+            if not races:
+                logger.error('Could not find races: {} {}'.format(day[0], day[1]))
+            
+            bets.exacta(races, stata, [1,2], 'All', diff)
+            bets.exacta(races, statb, [2,3], 'All', diff)
+        stata_list.append(stata)
+        statb_list.append(stata)
 
-    statc.printStats()
-    statd.printStats()
+    stata.printStats()
+    statb.printStats()
     
 
 if __name__ == '__main__':
